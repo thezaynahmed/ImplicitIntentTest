053f64279d10d52820d3c7dfa2c1e4e2173b741c
zaina <zainahmedpk12@outlook.com>
2020-08-03 22:34:50 +0500

    Initial commit

---
+++ app/src/main/java/com/zain/implicitintenttest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.zain.implicitintenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button =  findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "This is the message from implicit intent");
+                intent.putExtra(Intent.EXTRA_SUBJECT,"Do not worry! This works");
+                startActivity(intent);
+
+            }
+        });
+    }
+}
